be4e6e68ad8ffa8ff1e49d6aa984704d2c5960ec
Matheus Reis <souzareis42@gmail.com>
2026-09-10 18:50:11 -0300

    feat:  estrutura basica HTML (header, nav, tabela, footer)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Sobre } from './sobre/sobre';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "sobre", component:Sobre},
+    {path: "sobre-minha-loja", component:Sobre},
+    {path: "", component:Sobre}
+];
